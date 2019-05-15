f6ca5962f71d861db4cbef57907ddd28542be8c0
Harry beckwith <harry.beckwith@itech.media>
2019-05-15 18:04:32 +0100

    next build app

---
+++ static/gallery/london.js
@@ -0,0 +1,14 @@
+const gallery = [
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '1' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '2' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '1' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '2' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '1' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '2' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '1' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '2' },
+    { thumbnail: 'static/images/blog/example1.jpg', lightbox: 'static/images/blog/example1.jpg', alt: "img", caption: '2' }
+
+]
+
+export default gallery;
